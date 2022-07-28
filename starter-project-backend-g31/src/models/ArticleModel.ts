98f0eafe064c238da647d0b96391ee9f789f3c6a
Brook Zewdu <brookzewdu09@gmail.com>
2022-07-29 01:08:18 +0300

    create ArticleModel.ts

---
+++ starter-project-backend-g31/src/models/ArticleModel.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+
+const articleSchema = new mongoose.Schema({
+    Author: {
+        type: String,
+        minlength: 5,
+        maxlength:50,
+        required: true
+    },
+    Content: {
+        type: String,
+        minLength: 50,
+        maxLength: 10000,
+        required: true,
+    },
+    Rating: {
+        type: Number,
+        min: 0,
+        max: 5,
+        required: true
+    },
+    Comment: {
+        type: [ String ],
+        maxlength: 5000,
+        minlength: 1
+    },
+    postDate: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+
+module.exports = mongoose.model('Article', articleSchema);
