80dca109cd533e03aca9e30c817e74553d6b03f2
Abel-Mekonen <abelmekonen559@gmail.com>
2022-07-30 01:14:02 +0300

    create test for comment

---
+++ starter-project-backend-g32/src/tests/__tests__/comment.test.ts
@@ -0,0 +1,131 @@
+import {connect , clear, disconnect} from  '../setupdb';
+import app from '../../app';
+import mongoose from 'mongoose';
+
+const request = require('supertest')(app);
+const comment = require('../../models/comment');
+jest.setTimeout(30000);
+
+let commentId : String;
+
+beforeAll(async () => {
+    await connect();
+});
+
+beforeEach(async () => {
+    await comment.create({
+        content: 'test comment',
+        createdAt: new Date()
+    }).then((result: any) => {
+        commentId = result._id;
+    }
+    );
+});
+
+afterEach(async () => {
+    await clear();
+});
+
+afterAll(async () => {
+    await disconnect();
+});
+
+describe('Test for comment', () => {
+    it('should create a comment', async () => {
+        const response = await request.post('/api/v1/comment').send({
+            content: 'This is a test comment',
+        });
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body.message).toBe('Comment added successfully');
+    })
+
+    it('shouldn\'t create a comment without content', async () => {
+        const response = await request.post('/api/v1/comment').send({});
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('error');
+    })
+
+    it('should get a comment', async () => {
+        const response = await request.get('/api/v1/comment/' + commentId);
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('content');
+        expect(response.body.content).toBe('test comment');
+    })
+
+    it('shouldn\'t get a comment with invalid id', async () => {
+        const response = await request.get('/api/v1/comment/' + 'invalid id');
+        expect(response.status).toBe(404);
+        expect(response.body).toHaveProperty('error');
+    }
+    )
+
+    it('should get all comments', async () => {
+        const response = await request.get('/api/v1/comment');
+        expect(response.body).toEqual(
+            expect.arrayContaining([expect.objectContaining({
+                content: expect.any(String),
+                createdAt: expect.any(String),
+            })])
+        );
+        expect(response.status).toBe(200);
+    })
+
+    it('should delete a comment', async () => {
+        const response = await request.delete('/api/v1/comment/${commentId}');
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body.message).toBe('Comment deleted successfully');
+    })
+
+    it('shouldn\'t delete a comment with invalid id', async () => {
+        const response = await request.delete('/api/v1/comment/' + 'invalid id');
+        expect(response.status).toBe(404);
+        expect(response.body).toHaveProperty('error');
+    }
+    )
+
+    it('should update a comment', async () => {
+        const response = await request.put('/api/v1/comment/${commentId}').send({
+            content: 'This is an updated test comment',
+        });
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body.message).toBe('Comment updated successfully');
+    })
+
+    it('shouldn\'t update a comment with invalid id', async () => {
+        const response = await request.put('/api/v1/comment/' + 'invalid id').send({
+            content: 'This is an updated test comment',
+        });
+        expect(response.status).toBe(404);
+        expect(response.body).toHaveProperty('error');
+    }
+    )
+
+    it('shouldn\'t update a comment without content', async () => {
+        const response = await request.put('/api/v1/comment/${commentId}').send({});
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('error');
+    }
+    )
+
+    it('should patch a comment', async () => {
+        const response = await request.patch('/api/v1/comment/${commentId}').send({
+            content: 'This is an updated test comment',
+        });
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body.message).toBe('Comment updated successfully');
+    })
+
+    it('shouldn\'t patch a comment with invalid id', async () => {
+        const response = await request.patch('/api/v1/comment/' + 'invalid id').send({
+            content: 'This is an updated test comment',
+        });
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('error');
+    })
+
+    
+})
