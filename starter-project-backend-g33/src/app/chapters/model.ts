227eb395877ac0c3db5511151b396c58a7412e10
YeabAM <yeabkalu@gmail.com>
2022-07-29 16:13:05 +0300

    Add timestamp and Set Viruals on

--- starter-project-backend-g33/src/app/chapters/model.ts
+++ starter-project-backend-g33/src/app/chapters/model.ts
@@ -5,7 +5,10 @@ const ChapterSchema: Schema = new Schema({
   name: { type: String, required: true, unique: true },
   hoe: { type: String, required: true },
   deputyHoe: { type: String, required: true },
-  location : {type: String, required: true}
-});
+  location : {type: String, required: true},
+  
+  timestamps : {createdAt: 'created_at', updatedAt: 'modified_at' }
 
+});
+ChapterSchema.set('toJSON', {virtuals:true})
 export default mongoose.model<IChapterInterface>('Chapter', ChapterSchema);
