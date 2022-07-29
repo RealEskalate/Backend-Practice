20dad6c4761a10738c50d5cebf276d39adfdc235
YeabAM <yeabkalu@gmail.com>
2022-07-29 16:20:58 +0300

    Edit Schema

--- starter-project-backend-g33/src/app/chapters/model.ts
+++ starter-project-backend-g33/src/app/chapters/model.ts
@@ -5,10 +5,14 @@ const ChapterSchema: Schema = new Schema({
   name: { type: String, required: true, unique: true },
   hoe: { type: String, required: true },
   deputyHoe: { type: String, required: true },
-  location : {type: String, required: true},
-  
-  timestamps : {createdAt: 'created_at', updatedAt: 'modified_at' }
+  location : {type: String, required: true}
 
+},
+{
+     timestamps : {createdAt: 'created_at', updatedAt: 'modified_at' }
+     
 });
+
 ChapterSchema.set('toJSON', {virtuals:true})
+
 export default mongoose.model<IChapterInterface>('Chapter', ChapterSchema);
