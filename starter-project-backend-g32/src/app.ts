99b8e69211ed60663716e541de383ccead594c6f
Gadisa Amenu <amegadbay@gmail.com>
2022-07-30 17:07:28 +0300

    [backend] define routes

--- starter-project-backend-g32/src/app.ts
+++ starter-project-backend-g32/src/app.ts
@@ -1,6 +1,6 @@
 import express, {Application, Request, Response, NextFunction, json} from 'express';
 import dotenv from 'dotenv';
-
+import commentRouter from './routers/comment'
 const logger = require('morgan')
 // environmental variables configuration
 dotenv.config();
@@ -21,7 +21,8 @@ app.use("/", json({}))
 // app.use()
 
 
-
+// router used
+app.use(url+"/comments",commentRouter)
 
 app.get("/this",(req,res)=>{
     res.status(200).send("done")
--- starter-project-backend-g32/src/routers/comment.ts
+++ starter-project-backend-g32/src/routers/comment.ts
@@ -1,1 +1,13 @@
+import  { Router} from "express";
+import { addNewComment ,getCommentByID,deleteComment,updateComment,getAllComment} from "../controllers/commentController";
 
+const commentRouter = Router();
+
+
+commentRouter.get('/:id',getCommentByID)
+commentRouter.get('/', getAllComment)
+commentRouter.post('/', addNewComment)
+commentRouter.put('/:id',updateComment)
+commentRouter.delete('/:id',deleteComment)
+
+export default  commentRouter 
