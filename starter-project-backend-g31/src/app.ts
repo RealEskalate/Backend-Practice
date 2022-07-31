dee04e0c3ac3c2e5e5b1c4a84a655fc4ab9b07e2
Fetsum Abyu <eyosiasbitsu@gmail.com>
2022-08-01 01:17:12 +0300

    added midleware to app.ts

--- starter-project-backend-g31/src/app.ts
+++ starter-project-backend-g31/src/app.ts
@@ -5,9 +5,15 @@ dotenv.config();
 
 const app: Application = express();
 
+const articles = require('./routes/ArticleRoute');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api/articles', articles);
 
-app.use("/", json({}))
+app.get("/", (req, res) => {
+    res.send('hello!');
+});
+
 
 export default app
