7459c968fdcfd76a0f2de7be06fbd87450a164a4
ibsa21 <ibsaabraham663@gmail.com>
2022-08-01 12:03:00 +0300

    [starter-project-group-33] Implement user model

--- starter-project-backend-g33/src/app/users/model.ts
+++ starter-project-backend-g33/src/app/users/model.ts
@@ -1,20 +1,19 @@
-import mongoose, { Schema, Document, Mongoose } from 'mongoose'
-import IuserInterface from './interface'
-import IChapterInterface from '../chapters/interface'
+import mongoose, { Schema } from 'mongoose'
+import IUserInterface from './interface'
 
 const UserSchema: Schema = new Schema(
   {
-    name: { type: String, required: true},
-    email:{ type: String, required: true, unique: true},
-    bio:  { type: String },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    bio: { type: String },
     profileImage: { type: String },
     isActive: { type: Boolean, default: true },
-    chapter: {  type: Schema.Types.ObjectId, required: true }
+    chapter: { type: Schema.Types.ObjectId, required: true }
   },
-  
+
   { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
 )
 
 UserSchema.set('toJSON', { virtuals: true })
 
-export default mongoose.model<IuserInterface>('User', UserSchema)
+export default mongoose.model<IUserInterface>('User', UserSchema)
