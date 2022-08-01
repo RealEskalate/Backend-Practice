415be3b3ab747460c1868551d6e28a5e420e8048
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-08-01 16:06:36 +0300

    comment model

---
+++ starter-project-backend-g33/src/app/comments/model.ts
@@ -0,0 +1,27 @@
+import ICommentInterface from "./interface"
+import mongoose, {Schema} from 'mongoose'
+
+const CommentSchema = new mongoose.Schema({
+  articleId: {
+    type:  mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  commentContent: {
+    type: String,
+    required: true,
+  },
+  replies: {
+    type: String,
+  },
+  commentOwner: {
+    type:  String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+CommentSchema.set('toJSON',{virtuals: true})
+export default mongoose.model<ICommentInterface>('Comment', CommentSchema);
