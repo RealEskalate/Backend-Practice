3fad6f24ccafd408ae6451312ea5c140e40dc387
Brook Zewdu <brookzewdu09@gmail.com>
2022-08-01 10:39:20 +0300

    Refactor code

--- starter-project-backend-g31/src/app.ts
+++ starter-project-backend-g31/src/app.ts
@@ -1,10 +1,10 @@
 import express, {Application, Request, Response, NextFunction, json} from 'express';
 import dotenv from 'dotenv';
+import article from './routes/ArticleRoute';
 
 dotenv.config();
 
 const app: Application = express();
-const article = require('./routes/ArticleRoute');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api/articles', article);
