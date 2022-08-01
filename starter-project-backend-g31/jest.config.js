741cf7823b9e3463551b817fbed629c244a71a87
Feruz2 <feruzredi@gmail.com>
2022-08-01 10:59:55 +0300

    implement CRUD for comment

--- starter-project-backend-g31/jest.config.js
+++ starter-project-backend-g31/jest.config.js
@@ -3,9 +3,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.ts"],
-  verbose: true,
-  forceExit: true,
-  clearMocks: true,
-  resetMocks: true,
-  restoreMocks: true
+  
 };
