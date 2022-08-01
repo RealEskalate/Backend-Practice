bb44bf52566cae62ed36a56d9702b5ec42ab144c
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-08-01 11:05:42 +0300

    Update jest.config.js

--- starter-project-backend-g31/jest.config.js
+++ starter-project-backend-g31/jest.config.js
@@ -3,9 +3,4 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.ts"],
-  verbose: true,
-  forceExit: true,
-  clearMocks: true,
-  resetMocks: true,
-  restoreMocks: true
 };
