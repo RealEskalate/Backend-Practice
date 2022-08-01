8d7e78202b6dd059cc6aeae2a035c3df099d79ae
ibsa21 <ibsaabraham663@gmail.com>
2022-08-01 10:31:37 +0300

    Feat: added user model

---
+++ starter-project-backend-g33/src/app/users/model.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, Document, Mongoose } from 'mongoose'
+import IuserInterface from './interface'
+import IChapterInterface from '../chapters/interface'
+
+const UserSchema: Schema = new Schema(
+  {
+    name: { type: String, required: 'Name is required'},
+    email:{ type: String, required: true, unique: true},
+    bio:  { type: String },
+    profileImage: { type: String },
+    isActive: { type: Boolean, default: true },
+    chapter: {  type: Schema.Types.ObjectId, required: true }
+  },
+  
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
+)
+
+UserSchema.set('toJSON', { virtuals: true })
+
+export default mongoose.model<IuserInterface>('User', UserSchema)
