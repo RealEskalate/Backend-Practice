31d71fcde2608edba35eee249173a92ba6ff0ff0
Redi22 <redidemisse22@gmail.com>
2022-08-01 15:25:37 +0300

    (review change) removed try catch statements

--- starter-project-backend-g33/src/common/dal.ts
+++ starter-project-backend-g33/src/common/dal.ts
@@ -1,119 +1,56 @@
 import mongoose, { Model } from 'mongoose'
 
-
-
-const getOne = (
-    model:
-        Model<any, {}, {}>
-) => async (id: any) => {
-
-    try {
-        const doc = await model.findOne({ _id: id })
-        return doc
-    } catch (e) {
-        console.log(e)
-    }
+const getOne = (model: Model<any, {}, {}>) => async (id: any) => {
+  return await model.findOne({ _id: id })
 }
 
-const getAll = (
-    model:
-        Model<any, {}, {}>
-) => async (props: any) => {
-    try {
-        const filter = props
-        const docs = await model.find(filter)
-        return docs
-    } catch (e) {
-        console.log(e)
-    }
+const getAll = (model: Model<any, {}, {}>) => async (props: any) => {
+  return await model.find(props)
 }
 
-const createOne = (
-    model:
-        Model<any, {}, {}>
-) => async (props: any) => {
-    try {
-        const payload = props
-        const doc = await model.create(payload)
-        return doc
-    } catch (e) {
-        console.log(e)
-    }
+const createOne = (model: Model<any, {}, {}>) => async (props: any) => {
+  return await model.create(props)
 }
 
-const updateOne = (
-    model:
-        Model<any, {}, {}>
-) => async (props: any, id: String) => {
-    try {
-        const payload = props
+const updateOne =
+  (model: Model<any, {}, {}>) => async (props: any, id: String) => {
+    const payload = props
+    return await model.updateOne(
+      {
+        _id: id
+      },
+      { $set: { payload } }
+    )
+  }
 
-        const doc = await model
-            .updateOne(
-                {
-                    _id: id,
-                },
-                { $set: { payload }, }
-            )
+const clap = (model: Model<any, {}, {}>) => async (props: any) => {
+  const articleId = props.articleId
+  const userId = props.userId
 
-        return doc
-
-    } catch (e) {
-        console.log(e)
-    }
+  return await model.updateOne(
+    {
+      _id: articleId
+    },
+    { $push: { clappers: userId } }
+  )
 }
 
-const clap = (
-    model: Model<any, {}, {}>
-) => async (props: any) => {
-    try {
-        const articleId = props.articleId
-        const userId = props.userId
-
-        const doc = await model
-            .updateOne(
-                {
-                    _id: articleId,
-                },
-                { $push: { clappers: userId } }
-            )
-
-        return doc
-    } catch (e) {
-        console.log(e)
-    }
+const deleteOne = (model: Model<any, {}, {}>) => async (id: any) => {
+  return await model.updateOne(
+    {
+      _id: id
+    },
+    { $set: { isActive: false } }
+  )
 }
 
-const deleteOne = (
-    model: Model<any, {}, {}>
-) => async (id: any) => {
-    try {
-
-        const doc = await model
-            .updateOne(
-                {
-                    _id: id,
-                },
-                { $set: { isActive: false } }
-            )
-
-        return doc
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-const dataAccessLayer = (
-    model:
-        Model<any, {}, {}>
-) => ({
-    updateOne: updateOne(model),
-    getMany: getAll(model),
-    getOne: getOne(model),
-    createOne: createOne(model),
-    clap: clap(model),
-    deleteOne: deleteOne(model)
+const dataAccessLayer = (model: Model<any, {}, {}>) => ({
+  updateOne: updateOne(model),
+  getMany: getAll(model),
+  getOne: getOne(model),
+  createOne: createOne(model),
+  clap: clap(model),
+  deleteOne: deleteOne(model)
 })
 
 export default dataAccessLayer
-
