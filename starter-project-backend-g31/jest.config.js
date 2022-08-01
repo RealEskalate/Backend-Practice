31447405dd90ed1f1ae5e8ff9b539e5d2308d9c8
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-08-01 15:42:39 +0300

    Update jest.config.js

--- starter-project-backend-g31/jest.config.js
+++ starter-project-backend-g31/jest.config.js
@@ -3,8 +3,4 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.ts"],
-<<<<<<< HEAD
-  
-=======
->>>>>>> bb44bf52566cae62ed36a56d9702b5ec42ab144c
 };
