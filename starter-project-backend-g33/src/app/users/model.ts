f31196788e1f43fa7420cfe757575c24fa2b22c1
Gedion Teshome <Teshomegedion@gmail.com>
2022-08-01 15:52:02 +0300

    update model.ts with default validators and opts.

--- starter-project-backend-g33/src/app/users/model.ts
+++ starter-project-backend-g33/src/app/users/model.ts
@@ -3,12 +3,12 @@ import IUserInterface from './interface'
 
 const UserSchema: Schema = new Schema(
   {
-    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    bio: { type: String },
+    bio: { type: String, required: true },
     profileImage: { type: String },
     isActive: { type: Boolean, default: true },
-    chapter: { type: Schema.Types.ObjectId, required: true }
+    chapter: { type: Schema.Types.ObjectId, ref: 'Chapter', required: true }
   },
 
   { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
