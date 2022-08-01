3ca29d7298958b7ae67983866b49485c47f112e0
mintuka <mintuka2015@gmail.com>
2022-08-01 07:48:01 +0300

    [backend] crud for article

---
+++ starter-project-backend-g32/src/routes/article.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import {
+  getSpecificArticle,
+  getAllArticle,
+  createArticle,
+  updateArticle,
+  deleteArticle,
+} from '../controllers/article';
+const router = Router();
+
+router.route("/").post(createArticle).get(getAllArticle);
+router.route("/:rateID").get(getSpecificArticle).put(updateArticle).delete(deleteArticle);
+
+export default router;
