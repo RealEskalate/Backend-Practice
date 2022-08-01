cb341a4f59058beb94d72c6626f5402953ca15de
mintuka <mintuka2015@gmail.com>
2022-08-01 07:47:35 +0300

    [backend] crud for article

---
+++ starter-project-backend-g32/src/controllers/article.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from 'express'
+import Article from '../models/article'
+
+export const getSpecificArticle = async (req: Request, res: Response) => {
+        try {
+        const doc = await Article.findOne({ _id: req.params.id }).lean().exec();
+    
+        if (!doc) {
+            return res.status(404).end();
+        }
+        res.status(200).json({ data: doc });
+        } catch (e) {
+        console.error(e);
+        res.status(400).end();
+        }
+  };
+  
+export const getAllArticle = async (req: Request, res: Response) => {
+        try {
+        const docs = await Article.find().lean().exec();    
+        res.status(200).json({ data: docs });
+        } catch (e) {
+        console.error(e);
+        res.status(400).end();
+        }
+  };
+  
+export const createArticle = async (req: Request, res: Response) => {
+        try {
+        const doc = await Article.create({ ...req.body });
+        res.status(201).json({ data: doc });
+        } catch (e) {
+        console.error(e);
+        res.status(400).end();
+        }
+  };
+  
+export const updateArticle = async (req: Request, res: Response) => {
+        try {
+        const updatedDoc = await Article
+            .findOneAndUpdate(
+            {
+                _id: req.params.id,
+            },
+            req.body,
+            { new: true }
+            )
+            .lean()
+            .exec();
+    
+        if (!updatedDoc) {
+            return res.status(404).end();
+        }
+    
+        res.status(200).json({ data: updatedDoc });
+        } catch (e) {
+        console.error(e);
+        res.status(400).end();
+        }
+  };
+  
+export const deleteArticle = async (req: Request, res: Response) => {
+        try {
+        const removed = await Article.findOneAndRemove({
+            _id: req.params.id,
+        });
+    
+        if (!removed) {
+            return res.status(404).end();
+        }
+    
+        return res.status(200).json({ data: removed });
+        } catch (e) {
+        console.error(e);
+        res.status(400).end();
+        }
+  };
