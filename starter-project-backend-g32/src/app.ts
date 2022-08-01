79a38ff94eac234ee636776bf2bcd695f79f3b0f
NahomSeleshi <nahomseleshi43@gmail.com>
2022-08-01 11:07:58 +0300

    [backend] CRUD-for-user

--- starter-project-backend-g32/src/app.ts
+++ starter-project-backend-g32/src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import routes from "./routes";
+import userRouter from '../src/routes/user.routes'
 
 const app: Application = express();
 
@@ -9,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
+app.use('userProfile', userRouter);
 app.use("/users/:userID/posts/:postID/rates", routes.rateRouter);
+
 
 export default app;
