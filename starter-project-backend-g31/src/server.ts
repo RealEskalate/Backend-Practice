6b44280a92f2f4ca6e6f7e40d340ed95e0f42c69
Feruz2 <feruzredi@gmail.com>
2022-08-01 16:18:00 +0300

    implement CRUD for comment

--- starter-project-backend-g31/src/server.ts
+++ starter-project-backend-g31/src/server.ts
@@ -3,11 +3,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import { Console } from "console";
 dotenv.config();
-//require('dotenv').config();
 
 const PORT = process.env.PORT || 8000
 const DB_URI = process.env.MONGO_URI || "";
-//console.log(DB_URI,"EXDRFG")
 
 
 mongoose.connect(DB_URI, {
