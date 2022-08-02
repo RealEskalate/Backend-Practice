c75b21e1a8bce769e953dd10c97e2626656d3217
ibsa21 <ibsaabraham663@gmail.com>
2022-08-02 16:30:20 +0300

    [starter-project-g33] added data validator

--- starter-project-backend-g33/src/app/users/routes.ts
+++ starter-project-backend-g33/src/app/users/routes.ts
@@ -1,13 +1,13 @@
-import { Router, NextFunction, Request, Response } from 'express'
-import mongoose from 'mongoose'
+import { Router } from 'express'
 import controller from './controller'
+import { validateJoi, Schemas } from '../../middlewares/userJoi'
 
 const router = Router()
 
 // TODO: document with docstring
-router.get('/', controller.getAllUser)  
+router.get('/', controller.getAllUser)
 router.get('/:id', controller.getUser)
-router.post('/create', controller.create)
+router.post('/create', validateJoi(Schemas.user.create), controller.create)
 router.put('/:id', controller.updateUser)
 router.delete('/:id', controller.deleteUser)
 
