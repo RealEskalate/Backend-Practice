88bf95181f5245d3224c1773e9ecbbed4a90ddd9
ibsa21 <ibsaabraham663@gmail.com>
2022-08-02 14:29:22 +0300

    Minor change on chapter model and controller

--- starter-project-backend-g33/src/app/chapters/controller.ts
+++ starter-project-backend-g33/src/app/chapters/controller.ts
@@ -5,80 +5,82 @@ import Chapter from './model'
 const chapterDal = dataAccessLayer(Chapter)
 
 const getAllChapters = (req: Request, res: Response, next: NextFunction) => {
-
-    const filter = { isActive: true }
-    chapterDal.getMany(filter)
-        .then((data: any) => {
-            if (data.length() == 0) {
-                throw 'no chapter found';
-            }
-            res.status(200).json(data)
-
-        })
-        .catch(err => {
-            next(err)
-        })
-
+  const filter = { isActive: true }
+  chapterDal
+    .getMany(filter)
+    .then((data: any) => {
+      if (!data) {
+        throw 'no chapter found'
+      }
+      res.status(200).json(data)
+    })
+    .catch((err) => {
+      next(err)
+    })
 }
 
 const getChapter = (req: Request, res: Response, next: NextFunction) => {
-    chapterDal.getOne(req.params['id'])
-        .then(data => {
-            if (!data) {
-                throw 'No chapter by that ID is found'
-            }
-            res.status(200).json(data)
-        })
-        .catch(err => {
-            next(err)
-        })
+  chapterDal
+    .getOne(req.params['id'])
+    .then((data) => {
+      if (!data) {
+        throw 'No chapter by that ID is found'
+      }
+      res.status(200).json(data)
+    })
+    .catch((err) => {
+      next(err)
+    })
 }
 
 const updateChapter = (req: Request, res: Response, next: NextFunction) => {
-    const newChapter = req.body
-    chapterDal.updateOne(newChapter, newChapter.id)
-        .then(data => {
-            if (!data) {
-                throw 'No chapter with this ID'
-            }
-            res.status(200).json(data)
-        })
-        .catch(err => {
-            next(err)
-        })
+  const newChapter = req.body
+  chapterDal
+    .updateOne(newChapter, req.params.id)
+    .then((data) => {
+      if (!data) {
+        throw 'No chapter with this ID'
+      }
+      res.status(200).json(data)
+    })
+    .catch((err) => {
+      next(err)
+    })
 }
 
 const createChapter = (req: Request, res: Response, next: NextFunction) => {
-    const newChapter = req.body
-    chapterDal.createOne(newChapter)
-        .then(data => {
-            if (!data) {
-                throw 'No chapter with this ID'
-            }
-            res.status(200).json(data)
-        })
-        .catch(err => {
-            next(err)
-        })
+  const newChapter = req.body
+  chapterDal
+    .createOne(newChapter)
+    .then((data) => {
+      if (!data) {
+        throw 'No chapter with this ID'
+      }
+      res.status(200).json(data)
+    })
+    .catch((err) => {
+      next(err)
+    })
 }
 
 const disableChapter = (req: Request, res: Response, next: NextFunction) => {
-    chapterDal.deleteOne(req.params['id'])
-        .then(data => {
-            if (!data) {
-                throw 'Could not diable Chapter'
-            }
-            res.status(200).json(data)
-        })
-        .catch(err => {
-            next(err)
-        })
+  chapterDal
+    .deleteOne(req.params['id'])
+    .then((data) => {
+      if (!data) {
+        throw 'Could not diable Chapter'
+      }
+      res.status(200).json(data)
+    })
+    .catch((err) => {
+      next(err)
+    })
 }
 
 export default {
-    getAllChapters,
-    getChapter,
-    updateChapter,
-    createChapter,
-    disableChapter
+  getAllChapters,
+  getChapter,
+  updateChapter,
+  createChapter,
+  disableChapter
 }
