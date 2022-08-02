16faaf602a61b79f4f2bb87566b89befd35cac55
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-08-02 10:30:28 +0300

    Crud operation on user

--- starter-project-backend-g33/src/app/users/controller.ts
+++ starter-project-backend-g33/src/app/users/controller.ts
@@ -1,38 +1,114 @@
 import { NextFunction, Request, Response } from 'express'
 import mongoose from 'mongoose'
 import User from './model'
+import dataAccessLayer from '../../common/dal'
+
+const UserDAL = dataAccessLayer(User)
 
 // get all user
-const readAllUser = (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ message: 'all user' })
+const tester = (res , next, func, message, filter)=> {
+
+  func(filter)
+  .then((data:any)=> {
+    if (data.length == 0){
+      throw message
+    }
+    res.status(200).json(data);
+})
+.catch(err=>{
+  next(err)
+})
+}
+
+
+const getAllUser = (req: Request, res: Response, next: NextFunction) => {
+  const filter = {isActive: true}
+
+  UserDAL.getMany(filter)  
+    .then((data:any)=> {
+        if (data.length == 0){
+          throw 'No User Found'
+        }
+        res.status(200).json(data);
+    })
+    .catch(err=>{
+      next(err)
+    })
+
 }
 // get user by id
-const readOneUser = (req: Request, res: Response, next: NextFunction) => {
+const getAUser = (req: Request, res: Response, next: NextFunction) => {
   const userId = req.params.userId
-  res.status(200).send({ message: `user with ${userId}` })
+  UserDAL.getOne(userId).then((data:any)=>{
+    if (!data){
+      throw 'User Not Found'
+    }
+    res.status(200).json(data)
+  })
+  .catch(err=>{
+    next(err)
+  })
+
 }
 // create user
-const signUp = (req: Request, res: Response, next: NextFunction) => {
+const create = (req: Request, res: Response, next: NextFunction) => {
   //   console.log(req.body)
-  res.status(200).send({ message: 'user created ' })
+  const newUser = req.body
+  UserDAL.createOne(newUser)
+  .then( (data)=>{
+    if (!data){
+      throw ' Couldn\'t  create a new user'
+    }
+    res.status(200).json(data)
+
+  })
+  .catch( err =>{
+    next(err)
+  })
+  
 }
 // update user
-const updateOneUser = (req: Request, res: Response, next: NextFunction) => {
+const updateUser = (req: Request, res: Response, next: NextFunction) => {
   const userId = req.params.userId
-  res.status(200).send({ message: `user with ${userId} updated` })
+  const user = req.body
+  UserDAL.updateOne(user, userId)
+  .then( (data)=>{
+    if (!data){
+      throw ' Couldn\'t  Update the user'
+    }
+    res.status(200).json(data)
+
+  })
+  .catch( err =>{
+    next(err)
+  })
+
+
 }
 // delete user
-const deleteOneUser = (req: Request, res: Response, next: NextFunction) => {
+const deleteUser = (req: Request, res: Response, next: NextFunction) => {
   const userId = req.params.userId
-  res.status(200).send({ message: `user with ${userId} deleted` })
+  UserDAL.deleteOne(userId)
+  .then( (data)=>{
+    if (!data){
+      throw ' Couldn\'t  Delete the user'
+    }
+    res.status(200).json(data)
+
+  })
+  .catch( err =>{
+    next(err)
+  })
+
+
 }
 // login
 
 // export functions
 export default {
-  readAllUser,
-  readOneUser,
-  updateOneUser,
-  deleteOneUser,
-  signUp
+  getAllUser,
+  getAUser,
+  updateUser,
+  deleteUser,
+  create
 }
