22d8e538ed457267b7704c48ea2df9b9ce06aab7
Redi22 <redidemisse22@gmail.com>
2022-08-03 11:03:14 +0300

    (bugfix) fixed route bugs and schema array definitions

--- starter-project-backend-g33/src/common/routes.ts
+++ starter-project-backend-g33/src/common/routes.ts
@@ -4,14 +4,14 @@ const router: Router = Router()
 
 // Route imports
 import userRouter from '../resources/users/routes'
-// import commentRouter from '../app/comments/routes'
-// import articleRouter from '../app/articles/routes'
-// import chapterRouter from '../app/chapter/routes'
+import commentRouter from '../resources/comments/routes'
+import articleRouter from '../resources/articles/routes'
+import chapterRouter from '../resources/chapters/routes'
 
 // Higher route declaration
 router.use('/users', userRouter)
-// router.use('/comments', commentRouter)
-// router.use('/articles', articleRouter)
-// router.use('/chapters', chapterRouter)
+router.use('/comments', commentRouter)
+router.use('/articles', articleRouter)
+router.use('/chapters', chapterRouter)
 
 export = router
