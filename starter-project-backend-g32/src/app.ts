4163bb26b026c8f85a0ede978b77ab9a42a34fe1
NahomSeleshi <nahomseleshi43@gmail.com>
2022-08-03 12:47:44 +0300

    [backend] comments updated after a PR review

--- starter-project-backend-g32/src/app.ts
+++ starter-project-backend-g32/src/app.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import routes from "./routes";
 import articleRoutes from './routes/article'
-
+import userRoutes from './routes/user.routes'
 
 const app: Application = express();
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +13,7 @@ app.use("/" , json({}));
 app.use("/api/v1", require('../src/routes/routes'));
 app.use("/users/:userID/posts/:postID/rates", routes.rateRouter);
 app.use('/api/v1/articles', articleRoutes)
+app.use('/api/v1/userProfile', userRoutes)
 
 export default app;
 
