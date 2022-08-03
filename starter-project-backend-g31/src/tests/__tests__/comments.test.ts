98311eee253c55417b7cf1bf23b331d123b1d3f5
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-08-03 22:43:26 +0300

    interlink comment with user

--- starter-project-backend-g31/src/tests/__tests__/comments.test.ts
+++ starter-project-backend-g31/src/tests/__tests__/comments.test.ts
@@ -4,6 +4,11 @@ import app from '../../app';
 import {connect, clear, disconnect} from '../setupdb'
 import request from 'supertest'
 import {Comment} from '../../models/comment';
+import {userModel} from '../../models/user-model';
+import {Article} from '../../models/article';
+
+import bcrypt from 'bcrypt';
+import { getAllComments } from '../../controllers/comment';
 
 //connect to data base before starting any test
 beforeAll(async () => {
@@ -12,40 +17,67 @@ beforeAll(async () => {
 
 //disconect from database after any test
 afterAll(async ()=>{
+  await clear();
   await disconnect();
 })
 
 jest.setTimeout(30000);
 
-// dummy data to insert
-const dummy = {
-  author: "another person",
-  description: "another description"
-};
+
 
 // Test Suite on comments feature
 // describe('comment', () => {
 
 // Create an object id for dummy document to use for testing
 const commentId = mongoose.Types.ObjectId();
+const userId = mongoose.Types.ObjectId();
+// let userId: any;
+// let articleId: any;
+const articleId = mongoose.Types.ObjectId();
 
 // create a variable to hole a sample/dummy document
 let comment: any;
+let user: any;
+let article: any;
 
 // create and save sample document before any test
 beforeEach(async()=>{
-  
+  user = new userModel({
+    _id:userId,
+    name: 'Sasi',
+    email: 'sasi@gmail.com',
+    password: "password"
+  })
+  await user.save()
+  // userId = user._id;
+  const article = new Article({
+    _id : articleId,
+    author: 'sample author',
+    content: 'sample article 12345678901112131415',
+    rating: 4
+});
+  await article.save()
   comment = new Comment({
     _id:commentId,
-    author:"someone",
+    author:userId,
+    article: articleId,
     description:"test comment description",
   });
   await comment.save();
 });
-
+// dummy data to insert
+const dummy = {
+  description: "another description"
+};
 // remove sample document after any test to avoid object id redundancy 
 afterEach(async()=>{
-  await comment.remove({})
+  await Comment.collection.remove({})
+  await Article.collection.remove({})
+  await userModel.collection.remove({})
+  
+  // await comment.remove({});
+  // await user.remove({});
+  // await article.remove({});
 });
 
 
@@ -102,19 +134,18 @@ describe('GET: "/comment/:id" get a comment by id', ()=>{
 
 
 // check the add comment feature
-describe('POST: "/comment" Add new comment', ()=>{
+describe('POST: "/comment/:userId/:articleId" Add new comment', ()=>{
   
   // if author and description are properly filled
   
     
     // check status code and check response data
     it('status code should be 200', async ()=>{
-      const res = await request(app).post('/comment').send(dummy);
+      const res = await request(app).post(`/comment/${userId}/${articleId}`).send(dummy);
 
       expect(res.status).toBe(200);
       expect(
         {
-          author: res.body.author,
           description: res.body.description 
         }).toStrictEqual(dummy);
     });
@@ -136,18 +167,18 @@ describe('POST: "/comment" Add new comment', ()=>{
 
 
 //check the update comment by id feature
-describe('PATCH: "/comment/:id" update existing comment by id', ()=>{
+describe('PATCH: "/comment/commentId:/id:userId/:articleId" update existing comment by id', ()=>{
   
     
     // check status, content type, database
     it('return status code 200', async ()=>{
-      const res = await request(app).patch(`/comment/${commentId}`).send({
-        author: "changed"
+      const res = await request(app).patch(`/comment/${commentId}/${userId}/${articleId}`).send({
+        description: "changed"
       });
 
       const updatedcomment = await Comment.findById(commentId);
       
-      expect(updatedcomment.author).toStrictEqual("changed");
+      expect(updatedcomment.description).toStrictEqual("changed");
       expect(res.status).toBe(200);
       expect(res.headers['content-type']).toEqual(expect.stringContaining('json'));
       
@@ -177,7 +208,7 @@ describe('DELETE: "/comment" delete existing comment by id', ()=>{
 
     // Check status code and database
     it('return 200 status code', async ()=>{
-      const res = await request(app).delete(`/comment/${commentId}`); 
+      const res = await request(app).delete(`/comment/${commentId}/${userId}/${articleId}`); 
       
       const updatedcomment = await Comment.findById(commentId);
 
@@ -201,5 +232,3 @@ describe('DELETE: "/comment" delete existing comment by id', ()=>{
   
 })
 // });
-
-afterAll( async () => await disconnect());
