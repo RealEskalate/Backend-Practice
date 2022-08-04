85a2cd6b921118b9d7cb743fc989dc652034031b
Daniel Tefera Deti <danieltefera2211@gmail.com>
2022-08-04 11:07:56 +0300

    [backend] setting up the invironment

--- starter-project-backend-g32/src/app.ts
+++ starter-project-backend-g32/src/app.ts
@@ -2,7 +2,7 @@ import express, {Application, Request, Response, NextFunction, json} from 'expre
 import dotenv from 'dotenv';
 
 dotenv.config();
-import routes from "./routes";
+import routes from "./routes";//not clear
 import articleRoutes from './routes/article'
 
 
@@ -11,8 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/" , json({}));
 app.use("/api/v1", require('../src/routes/routes'));
-app.use("/users/:userID/posts/:postID/rates", routes.rateRouter);
+app.use("/users/:userID/posts/:postID/rates", routes.rateRouter);.//not clear
 app.use('/api/v1/articles', articleRoutes)
 
 export default app;
-
--- starter-project-backend-g32/src/routes/article.ts
+++ starter-project-backend-g32/src/routes/article.ts
@@ -5,11 +5,20 @@ import {
   createArticle,
   updateArticle,
   deleteArticle,
+
+  //to be implimented
+  getAllRating,
+  getSpecificRating,
+  createRating,
+  updateRating,
+  deleteRating,
 } from '../controllers/article'
 
 const router = Router()
 
 router.route("/").post(createArticle).get(getAllArticle)
 router.route("/:articleID").get(getSpecificArticle).put(updateArticle).delete(deleteArticle)
+router.route("/:articleID/rating").post(createRating).get(getAllRating)
+router.route("/:articleID/rating/:ratingId").get(getSpecificRating).put(updateRating).delete(deleteRating)
 
 export default router
