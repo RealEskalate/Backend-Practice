00c9e70ec8c8eefe1658670d5575ffef2ecf5f2c
mintuka <mintuka2015@gmail.com>
2022-08-05 15:12:04 +0300

    [backend] upload file to cloudinary

--- starter-project-backend-g32/src/tests/__tests__/upload.test.ts
+++ starter-project-backend-g32/src/tests/__tests__/upload.test.ts
@@ -1,11 +1,11 @@
 import Article from '../../models/article'
+import User from '../../models/user.models'
 import request from 'supertest'
-import mongoose from 'mongoose'
 import {connect, disconnect, clear} from '../setupdb'
 import app from '../../app'
 
 let articleID = ''
-
+let userID = ''
 beforeAll(async () => {
     await connect()
 })
@@ -18,6 +18,18 @@ beforeEach(async () => {
     const newArticle = new Article(article)
     await newArticle.save()
     articleID = newArticle._id
+
+    const user = {
+        firstName: "John",
+        lastName: "Doe",
+        email: "johnDoe@gmail.com",
+        password: "johnDoe",
+        profilePic: "linkToProfile",
+    }
+    const newUser = new User(user)
+    await newUser.save()
+    userID = newUser._id
+    
 })
 
 afterAll(async () => {
@@ -30,14 +42,22 @@ afterEach(async () => {
 
 describe('Integration testing for upload routes', () => {
 
-    it('Fails to upload file', async () => {
+    it('Fails to upload article file', async () => {
         const {body,statusCode} = await request(app).put(`/api/v1/upload/articleMedia/${articleID}`).send({})
         expect(statusCode).toBe(400)
     })
     
-    it('Fails to delete file',async () => {
+    it('Fails to delete article file',async () => {
         const {body,statusCode} = await request(app).delete(`/api/v1/upload/articlMedia/fakeId`)
         expect(statusCode).toBe(404)
     })
-
+    it('Fails to upload user profile', async () => {
+        const {body,statusCode} = await request(app).put(`/api/v1/upload/userProfile/${userID}`).send({})
+        expect(statusCode).toBe(400)
+    })
+    
+    it('Fails to delete user profile',async () => {
+        const {body,statusCode} = await request(app).delete(`/api/v1/upload/userProfile/fakeId`)
+        expect(statusCode).toBe(404)
+    })
 })
