dd30f8fb6c418f067cf7bf4b4ca65a2323c19620
ibsa21 <ibsaabraham663@gmail.com>
2022-08-05 15:13:40 +0300

    authorized routes

--- starter-project-backend-g33/src/resources/articles/routes.ts
+++ starter-project-backend-g33/src/resources/articles/routes.ts
@@ -1,19 +1,20 @@
 import { Router } from 'express'
 import articleController from './controller'
-
+import { auth } from '../../middlewares/auth'
 const router = Router()
 
 router
   .route('/')
-  .get(articleController.getAllArticles)
-  .post(articleController.createArticle)
-  .put(articleController.clap)
+  .get(auth, articleController.getAllArticles)
+  .post(auth, articleController.createArticle)
+  .put(auth, articleController.clap)
 
 router
   .route('/:id')
-  .get(articleController.getArticle)
-  .put(articleController.updateArticle)
-  .delete(articleController.deleteArticle)
+  .get(auth, articleController.getArticle)
+  .put(auth, articleController.updateArticle)
+  .delete(auth, articleController.deleteArticle)
 
-router.route('/author/:id').get(articleController.getMyArticle)
+router.route('/author/:id').get(auth, articleController.getMyArticle)
+router.route('/search/:title').get()
 export default router
