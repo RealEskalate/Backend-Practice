f7a1dfaa2e4d118471b5cdfe714342e570409998
GedionT <teshomegedion@gmail.com>
2022-08-05 11:36:19 +0300

    added authed user to req.body.user

--- starter-project-backend-g33/src/middlewares/auth.ts
+++ starter-project-backend-g33/src/middlewares/auth.ts
@@ -1,18 +1,22 @@
 // auth middleware with jwt
 import { Request, Response, NextFunction } from 'express'
 import { verify } from 'jsonwebtoken'
+import dataAccessLayer from '../common/dal'
 import User from '../resources/users/model'
+
+const UserDAL = dataAccessLayer(User)
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers['x-access-token'] || req.headers['authorization'] // Express headers are auto converted to lowercase
   if (token) {
     try {
       const decoded = verify(token, process.env.JWT_SECRET || 'secret')
-      const user = await User.findById(decoded.id)
+      const user = await UserDAL.getManyUserSecured({ _id: decoded.id })
       if (!user) {
         return res.status(401).json({ message: 'Not authorized' })
       }
-      req.user = user // TODO: fix ts-lint issue here
+
+      req.body.user = user
       next()
     } catch (err) {
       return res.status(401).json({ message: 'Not authorized' })
