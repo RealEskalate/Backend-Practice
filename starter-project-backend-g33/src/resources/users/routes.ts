2685c83c16e384f5228a2c4a9c77d8eec699dfa9
GedionT <teshomegedion@gmail.com>
2022-08-05 11:36:34 +0300

    placed auth guards to critical routes

--- starter-project-backend-g33/src/resources/users/routes.ts
+++ starter-project-backend-g33/src/resources/users/routes.ts
@@ -1,19 +1,20 @@
 import { Router } from 'express'
 import userController from './controller'
 import { validateJoi, Schemas } from '../../middlewares/validateJoi'
+import { auth } from '../../middlewares/auth'
 
 const router = Router()
 
 router
   .route('/')
-  .get(userController.getAllUser)
+  .get(auth, userController.getAllUser)
   .post(validateJoi(Schemas.user.create), userController.create)
 
 router
   .route('/:id')
-  .get(userController.getUser)
-  .put(validateJoi(Schemas.user.create), userController.updateUser)
-  .delete(userController.deleteUser)
+  .get(auth, userController.getUser)
+  .put(auth, validateJoi(Schemas.user.create), userController.updateUser)
+  .delete(auth, userController.deleteUser)
 
 router.route('/login').post(userController.login)
 
